b08168b2bff274f2344eb2b424fd1e6a5c6462f0
mboysan <mboysan.git@gmail.com>
2018-03-30 20:57:25 +0300

    refs #8 - testing iter1

--- slurm/mpi.sh
+++ slurm/mpi.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 cd ..
 cd ./target
+
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+
+module load openmpi-3.0.0
 
 echo "slurm/mpi init"
 mpirun -np 1 java -cp *jar-with-dependencies.jar MPIMain $@
