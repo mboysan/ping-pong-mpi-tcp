63a48f006dcefbbf3748402b8b8073fa34eb666b
mboysan <mboysan.git@gmail.com>
2018-04-02 23:54:54 +0300

    refs #8 - collecting slurm data

---
+++ slurm/data/tcp/m3tcp_2_1/tcp_config.sh
@@ -0,0 +1,1 @@
+0-1 java -cp ../ping-pong-mpi-tcp-1.0-SNAPSHOT-jar-with-dependencies.jar TCPMainMultiJVM 2 %t false 8002
---
+++ slurm/data/tcp/m3tcp_4_1/tcp_config.sh
@@ -0,0 +1,1 @@
+0-3 java -cp ../ping-pong-mpi-tcp-1.0-SNAPSHOT-jar-with-dependencies.jar TCPMainMultiJVM 4 %t false 8004
