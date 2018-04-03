364853360447dfca6134bcaf96390347e6987265
mboysan <mboysan.git@gmail.com>
2018-04-03 21:14:03 +0300

    Revert #22 - reuse sync block for MPI.COMM_WORLD object. And trying iSend now.

--- mpi.sh
+++ mpi.sh
@@ -5,5 +5,5 @@ mvn clean install
 
 # go to target and run MPIMain class
 cd ./target
-count=1
-mpirun --oversubscribe -np $count java -cp *jar-with-dependencies.jar MPIMain 2 true
+count=5
+mpirun --oversubscribe -np $count java -cp *jar-with-dependencies.jar MPIMain 2 false
