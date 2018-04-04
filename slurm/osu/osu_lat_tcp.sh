0afa19e487f036edc2e3850e07721d8b5c756c6b
mboysan <mboysan.git@gmail.com>
2018-04-04 22:30:39 +0300

    refs #24 - add osu_latency_tcp slurm script

---
+++ slurm/osu/osu_lat_tcp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p testing
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH -t 00:01:00
+
+module load jdk-1.8.0_25
+module load openmpi-1.8.4
+
+echo "osu_latency test with java TCP"
+mpirun java -cp ../ping-pong-mpi-tcp-1.0-SNAPSHOT-jar-with-dependencies.jar testframework/osu/pt2pt/OSULatencyTCP
