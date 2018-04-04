ded14af55856f9df27fe32593892bf4290e64fa3
mboysan <mboysan.git@gmail.com>
2018-04-04 11:58:07 +0300

    refs #8 - collected slurm results

---
+++ slurm/data/m0mpi_3_1/_bak/mpi0_3_1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+#SBATCH -N 3
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH -t 00:01:00
+#SBATCH --mem=25000
+module load jdk-1.8.0_25
+module load openmpi-1.8.4
+mpirun -np 3 java -cp ../ping-pong-mpi-tcp-1.0-SNAPSHOT-jar-with-dependencies.jar MPIMain 3 false
+#!/bin/bash 
+#SBATCH -N 3
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH -t 00:01:00
+#SBATCH --mem=25000
+module load jdk-1.8.0_25
+module load openmpi-1.8.4
+mpirun -np 3 java -cp ../ping-pong-mpi-tcp-1.0-SNAPSHOT-jar-with-dependencies.jar MPIMain 1 false
---
+++ slurm/data/m0tcp_5_1/tcp_config.sh
@@ -0,0 +1,1 @@
+0-4 java -cp ../ping-pong-mpi-tcp-1.0-SNAPSHOT-jar-with-dependencies.jar TCPMainMultiJVM 5 %t false 9095
